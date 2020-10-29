f27de6029cc5d12e16b5ddf675717ddab96b89d6
Henro Potgieter <henro47@gmail.com>
2020-10-29 18:09:29 +0200

    Added ID validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormControl, Validators } from '@angular/forms';
 import { faFileUpload, faInfoCircle} from '@fortawesome/free-solid-svg-icons';
+import { findReadVarNames } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-home',
@@ -40,6 +41,41 @@ export class HomeComponent implements OnInit {
     Validators.required,
     Validators.minLength(2)
   ]);
+
+  validateID(idNumber:String)
+  {
+    var evenSum = "";
+    var oddSum = 0 ;
+    let multiSum = 0 ;
+    for(let i =0;i<idNumber.length-1;i++)
+    {
+      var numAtIndex = parseInt(idNumber[i]);
+      if(i%2 ==0)
+      {
+        oddSum += numAtIndex;
+      }
+      else
+      {
+        evenSum += numAtIndex
+        console.log(evenSum);
+      }
+    }
+
+    multiSum = parseInt(evenSum) * 2 ;
+    let finalSum = 0 ;
+
+    for(let i =0; i< multiSum.toString().length;i++)
+    {
+      var numAtIndex = parseInt(multiSum.toString()[i]);
+      finalSum += numAtIndex;
+    }
+
+    if((oddSum + finalSum)%10==0)
+    {
+      return true;
+    }
+    return false;
+  }
 
   csvInputChange(fileInputEvent: any) {
     console.log(fileInputEvent.target.files[0]);
@@ -117,27 +153,31 @@ export class HomeComponent implements OnInit {
     let con = (document.getElementById("contact") as HTMLInputElement).value;
     let nat = (document.getElementById("nat") as HTMLInputElement).value;
 
-    let userEmail = localStorage.getItem('email');
-    let token = 'Bearer ' ;
-    token += localStorage.getItem('token'); 
-
-    let httpHeaders = new HttpHeaders()
-    .set('Authorization', token);
-
-    var data = [
-      {'propName' : 'idNum', 'value': id},
-      {'propName' : 'fName', 'value': firstName},
-      {'propName' : 'lName', 'value': lastName},
-      {'propName' : 'contact', 'value': con},
-      {'propName' : 'national', 'value': nat}
-    ]
-
-    console.log("data:" + data);
-
-    this.http.patch('https://project-2-api-hfr.herokuapp.com/user'+ userEmail, data ,{headers: httpHeaders})
-    .subscribe(Response => {
-      console.log(Response);
-    });
+    if(this.validateID(id))
+    {
+      let userEmail = localStorage.getItem('email');
+      let token = 'Bearer ' ;
+      token += localStorage.getItem('token'); 
+  
+      let httpHeaders = new HttpHeaders()
+      .set('Authorization', token);
+  
+      var data = [
+        {'propName' : 'idNum', 'value': id},
+        {'propName' : 'fName', 'value': firstName},
+        {'propName' : 'lName', 'value': lastName},
+        {'propName' : 'contact', 'value': con},
+        {'propName' : 'national', 'value': nat}
+      ]
+  
+      console.log("data:" + data);
+      console.log('ID IS VALID');
+  
+      this.http.patch('https://project-2-api-hfr.herokuapp.com/user'+ userEmail, data ,{headers: httpHeaders})
+      .subscribe(Response => {
+        console.log(Response);
+      });
+    }
   }
   
 
